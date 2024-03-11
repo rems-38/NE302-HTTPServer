67c8c401ff179324e1d1528f782365283548cae4
arthurp79 <154096817+arthurp79@users.noreply.github.com>
2024-03-11 13:28:43 +0000

    On essaye de lire la grammaire et de la classifier, c'est chaud

--- documents/simplegrammar/sg.c
+++ documents/simplegrammar/sg.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <string.h>
 
 #define GRAMMAIRE "test.abnf"
 #define INIT message
@@ -10,28 +11,36 @@ int main(void) {
     FILE *gram = fopen(GRAMMAIRE, "r");
     char *line = NULL;
     size_t len = 0;
-    ssize_t read;
+    size_t read;
 
     if (gram == NULL) {
         printf("Impossible d'ouvrir le fichier %s\n", GRAMMAIRE);
         return -1;
     }
-    
+
     while ((read = getline(&line, &len, gram)) != -1) {
         if (line[0] == ';') { continue; }
+
+        char *ligne_split;
         
-        printf("%s", line);
+        ligne_split = strtok(line," = ");
 
 
+        printf("%d : %d\n",ligne_split[0],ligne_split[1]);
+       /* char *type = ligne_split[0];
 
+        int i = 2;
+        while (ligne_split[i] != '\0'){
 
+        }
+*/
+        printf("%s\n", line);
 
-
-
-    }
+        }
+    
 
     fclose(gram);
     if (line) { free(line); }
 
     return 0;
-}
+} 
