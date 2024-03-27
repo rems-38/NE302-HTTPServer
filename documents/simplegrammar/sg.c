de8748523317acb3cbda4a804cf8eb509bbd62a3
arthurp79 <154096817+arthurp79@users.noreply.github.com>
2024-03-27 11:58:17 +0000

    création main verif.c

--- documents/simplegrammar/sg.c
+++ documents/simplegrammar/sg.c
@@ -30,45 +30,6 @@ typedef struct Element {
 */
 
 
-int verifMessage(char *text, Element *data) {
-    size_t curr = 0;
-
-    char *starting = malloc(5 * sizeof(char));
-    strncpy(starting, text, 5);
-    
-    if (verifDebut(starting) == false) return -1;
-    ajoutFils(data, starting, len);
-    curr += len;
-
-    
-
-}
-
-
-bool verifDebut(char *text){
-    if (strcmp(text, "start")) return true;
-    return false;
-}
-
-/*bool verifMot(char *text){
-    char *textbis = *text
-    while(isSeparateur(textbis)==false){
-        *textbis += sizeof(char);
-    }
-    if(verifALPHA(text) && verifSeparateur(textbis)){ return true;}
-    return false
-}
-
-bool isSeparateur(char *text){
-    if (strcmp(text[0]," ")||strcmp(text[0],"   ")||strcmp(text[0],"-"||strcmp(text[0],"_")){return true;}
-    return false
-}
-
-bool verifSeparateur(char *text)
-
-
-*/
-
 int main(int argc, char *argv[]) {
     
     FILE *ftest = fopen(argv[1], "r");
@@ -76,7 +37,7 @@ int main(int argc, char *argv[]) {
     size_t len = 0;
     ssize_t read;
 
-    element *message = malloc(sizeof(element));
+    Element *message = malloc(sizeof(Element));
 
     if (ftest == NULL) {
         printf("Impossible d'ouvrir le fichier %s\n", argv[1]);
--- documents/simplegrammar/verif.c
+++ documents/simplegrammar/verif.c
@@ -1,107 +1,132 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-
-int verifMessage(char *text, Element *data) {
-    size_t curr = 0;
-
-    char *starting = malloc(5 * sizeof(char));
-    strncpy(starting, text, 5);
-    
-    if (verifDebut(starting) == false){return -1};
-
-    ajoutFils(data, starting, len);
-    curr += len;
-
-    int i = 0;
-    bool boucle = true;
-
-    while(boucle){
-    if(verifMot(text+curr,curr)){
-        if(verifPonct(text+curr,curr)){
-            i++;
-        }
-        else{
-            boucle = false;
-        }
-    }
-    else if (verifNombre(text+curr,curr)){
-        if(isSeparateur(text+curr,curr)){
-            i++;
-        }
-        else{
-            boucle = false;
-        }
-    }
-    else{
-        boucle = false;
-    }
-
-    if(i<2){return false;} // vérifier si on a au moins 2 séquences
-
-    while(isPonct(text+curr,curr)){ // optionnel
-        curr += sizeof(char);
-    }
-
-    // Manque fin LF
+bool verifDebut(char *text) {
+    return strcmp(text, "start") == 0;
 }
 
-bool verifDebut(char *text){
-    if (strcmp(text, "start")) return true;
-    return false;
+bool verifFin(char *text) {
+    return strcmp(text, "fin") == 0;
 }
 
-bool verifMot(char *text,size_t curr){
+bool isPonct(char c) {
+    return (c == ',' || c == '.' || c == '!' || c == '?' || c == ':');
+}
 
-    curr_comp = curr;
+bool isSeparateur(char c) {
+    return (c == ' ' || c == '-' || c == '_');
+}
 
-    while(isALPHA(text+curr)){ 
-        curr += sizeof(char);
-        }
+bool isAlpha(char c) {
+    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
 
-    if (curr == curr_comp){
-        return false;
+bool isDigit(char c) {
+    return (c >= '0' && c <= '9');
+}
+
+
+bool verifMot(char *text, size_t curr) {
+    size_t curr_comp = curr;
+
+    while (isAlpha(text[curr])) {
+        curr++;
     }
 
-
-    if (isSeparateur(text+curr)==false){
+    if (curr == curr_comp)
         return false;
-    }
+
+    if (!isSeparateur(text[curr]))
+        return false;
 
     return true;
 }
 
-bool verifPonct(char *text,size_t curr){
+bool verifNombre(char *text, size_t curr) {
+    int i = 0;
 
+    while (isDigit(text[curr])) {
+        i++;
+        curr++;
+    }
+
+    if (i <= 1)
+        return false;
+
+    return true;
 }
 
-bool verifNombre(char *text,size_t curr){
+bool verifMessage(char *text) {
 
+    size_t curr = 0; // curseur qui permet de parcourir la ligne sans modifier le pointeur texte initial
+
+    char *starting = malloc(5 * sizeof(char)); // on crée une chaine de caractère pour stocker les 5 premiers caractères de notre ligne
+    strncpy(starting, text, 5); // on copie le contenu des 5 premier caractères de la ligne
+    
+    if (!verifDebut(starting)) // on vérifie si notre ligne commence par "start"
+        return false;
+
+    curr += 5; // on a vérifié "start", on passe au 6ième caractère de la ligne avec +5 car sizeof(char) = 1
+
+    int i = 0; // indicateur pour vérifier qu'on a bien au moins 2 séquences (mot ponct/nombre separateur)
+    bool boucle = true; // passe à false dès qu'on a plus de séquences
+
+    while (boucle) { // vérifier si on a au moins 2 séquences (mot ponct/nombre separateur)
+        if (verifMot(text, curr)) {
+            if (isPonct(text[curr])) {
+                i++;
+            } else {
+                return false;
+            }
+        } else if (verifNombre(text, curr)) {
+            if (isSeparateur(text[curr])) {
+                i++;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    if (i < 2) // vérifiér qu'on ai au moins 2 séquences
+        return false;
+
+    while (isPonct(text[curr])) { // optionnel on parcoure la ligne jusqu'à ce qu'il n'y ait plus de ponct
+        curr++;
+    }
+
+    char *fin = malloc(3 * sizeof(char)); // on vérifie si le message se termine par "fin" de la même manière qu'on a vérifié "début"
+    strncpy(fin, text + curr, 3);
+    
+    if (!verifFin(fin))
+        return false;
+
+    curr += 3;
+
+    if (text[curr] != 10) // LF = Line feed (retour à la ligne) vaut 10 en ASCII
+        return false;
+
+    return true; // notre message est correcte
 }
 
-// Verif caractère par caractère
+int main(int argc, char *argv[]) {
+    
+    if(argc != 2){
+        printf("Erreur d'argument\n");
+        return 0;
+    }
 
-bool isSeparateur(char *text){
-    if (strcmp(text[0]," ")||strcmp(text[0],"   ")||strcmp(text[0],"-"||strcmp(text[0],"_"))){return true;}
-    return false
+    FILE *ftest = fopen(argv[1], "r");
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    
+    if ((read = getline(&line, &len, ftest)) != -1) { 
+        printf("Verification du message : %d\n", verifMessage(line));
+    }
+    
+    return 0;
 }
-
-bool isAlpha(char *text){
-    if ((*text >= 65 && *text <= 90)||(*text >= 97 && *text <= 122)){ // table ascii : 65-90 = A-Z et 97-122 = a-z
-        return true;}
-    return false
-}
-
-bool isDigit(char *text){
-    if (*text >= 48 && *text <= 57){ // table ascii : 48-57 = 0-9
-        return true;}
-    return false
-}
-
-/*
-Table ASCII :
-SP = 32
-HTAB = 9 
-LF = 10
-*/
-
