f5ffef3553d303d641aa6f368e389be88a77ce84
rems-38 <rem.mazzone@orange.fr>
2024-03-27 13:40:15 +0100

    code working on test0.txt file

--- documents/simplegrammar/sg.c
+++ documents/simplegrammar/sg.c
@@ -10,6 +10,23 @@
 #define HTAB '\t'
 #define LF '\n'
 
+/*
+Table ASCII :
+A-Z = 65-90
+a-z = 97-122
+0-9 = 48-57
+SP = 32
+HTAB = 9
+- = 45
+_ = 95
+, = 44
+. = 46
+! = 33
+? = 63
+: = 58
+LF = 10
+*/
+
 typedef struct Element {
     char *word;
     size_t length;
@@ -29,45 +46,106 @@ typedef struct Element {
 }
 */
 
-
-int verifMessage(char *text, Element *data) {
-    size_t curr = 0;
-
-    char *starting = malloc(5 * sizeof(char));
-    strncpy(starting, text, 5);
-    
-    if (verifDebut(starting) == false) return -1;
-    ajoutFils(data, starting, len);
-    curr += len;
-
-    
-
+bool isDebut(char *text){
+    return strcmp(text, "start");
 }
 
+bool isAlpha(char text){
+    return (text >= 65 && text <= 90) || (text >= 97 && text <= 122);
+}
 
-bool verifDebut(char *text){
-    if (strcmp(text, "start")) return true;
+bool isDigit(char text) {
+    return (text >= 48 && text <= 57);
+}
+
+bool isSeparateur(char text) {
+    return (text == 32 || text == 9 || text == 45 || text == 95);
+}
+
+bool isPonct(char text) {
+    return (text == 44 || text == 46 || text == 33 || text == 63 || text == 58);
+}
+
+bool isFin(char *text) {
+    return strcmp(text, "fin");
+}
+
+bool isLF(char text) {
+    return (text == 10);
+}
+
+bool isMot(char *text, size_t *curr){
+    size_t icurr = 0;
+
+    while (isAlpha(*(text+icurr))) { 
+        icurr += 1;
+    }
+
+    if (icurr != 0) {
+        if (isSeparateur(*(text+icurr))) {
+            *curr += icurr;
+            *curr += 1;
+            return true;
+        }
+    }
+
     return false;
 }
 
-/*bool verifMot(char *text){
-    char *textbis = *text
-    while(isSeparateur(textbis)==false){
-        *textbis += sizeof(char);
+bool isNombre(char *text, size_t *curr){
+    size_t icurr = 0;
+    while (isDigit(*(text+icurr))) {
+        icurr += 1;
     }
-    if(verifALPHA(text) && verifSeparateur(textbis)){ return true;}
-    return false
+    *curr += icurr;
+    return icurr != 0;
 }
 
-bool isSeparateur(char *text){
-    if (strcmp(text[0]," ")||strcmp(text[0],"   ")||strcmp(text[0],"-"||strcmp(text[0],"_")){return true;}
-    return false
+int verifMessage(Element *data) {
+    size_t curr = 0;
+    
+    if (isDebut(data->word) == false) { return -1; }
+
+    // ajoutFils(data, starting, len);
+    curr += 5;
+
+    int i = 0;
+    bool boucle = true;
+
+    while(boucle) {
+        if (isMot(data->word+curr, &curr)) {
+            if (isPonct(*(data->word+curr))) {
+                curr++;
+                i++;
+            }
+            else { boucle = false; }
+        }
+        else if (isNombre(data->word+curr, &curr)) {
+            if (isSeparateur(*(data->word+curr))) {
+                curr++;
+                i++;
+            }
+            else { boucle = false; }
+        }
+        else { boucle = false; }
+
+        // if (i < 2) { return false; } // vérifier si on a au moins 2 séquences
+
+        // while(isPonct(data->word+curr,curr)){ // optionnel
+        //     curr += 1;
+        // }
+        
+    }
+
+    if (isFin(data->word+curr) == false) { return -1; }
+    curr += 3;
+    
+    if (isLF(*(data->word+curr)) == false) { return -1; }
+    curr += 1;
+    
+    return 1;
 }
 
-bool verifSeparateur(char *text)
-
-
-*/
 
 int main(int argc, char *argv[]) {
     
@@ -76,7 +154,7 @@ int main(int argc, char *argv[]) {
     size_t len = 0;
     ssize_t read;
 
-    element *message = malloc(sizeof(element));
+    Element *message = malloc(sizeof(Element));
 
     if (ftest == NULL) {
         printf("Impossible d'ouvrir le fichier %s\n", argv[1]);
@@ -86,7 +164,8 @@ int main(int argc, char *argv[]) {
     if ((read = getline(&line, &len, ftest)) != -1) {
         message->word = line;
         message->length = read; // 27 (26 + 1)
-        verifMessage(line, message);
+        int output = verifMessage(message);
+        printf("output : %d\n", output);
     }
 
     fclose(ftest);
